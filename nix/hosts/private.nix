9b515b4bb20bbe62908a8038f35ec96000c4de31
nabekou29 <nabekou0729@gmail.com>
2026-04-15 23:31:46 +0900

    nix: private プロファイルを追加

---
+++ nix/hosts/private.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  homebrew.casks = [
+    "autodesk-fusion"
+    "bambu-studio"
+    "blender"
+    "parsec"
+    "steam"
+  ];
+
+  homebrew.masApps = {
+    "LINE" = 539883307;
+  };
+}
