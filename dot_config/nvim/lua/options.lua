bd4a6af494751d73ef52f1d47663f0dc79396366
nabekou29 <nabekou0729@gmail.com>
2026-06-03 14:04:03 +0900

    nvim: deprecated な vim.highlight.on_yank を vim.hl.on_yank に置換
    Neovim 0.11 で vim.highlight が vim.hl にリネームされたため。

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -97,7 +97,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 -- Yank した場所をハイライト
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+    vim.hl.on_yank({ higroup = "IncSearch", timeout = 200 })
   end,
 })
 
